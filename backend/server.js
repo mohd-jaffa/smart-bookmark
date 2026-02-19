8994379ecda2109644de1521e1db5dd728112086
jaFFa <jafakash.14@gmail.com>
2026-02-19 21:14:36 +0530

    minor fix

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const { app } = require("./app");
+const { app } = require("./src/app");
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Backend listening on port ${port}`);
+    // eslint-disable-next-line no-console
+    console.log(`Backend listening on port ${port}`);
 });
